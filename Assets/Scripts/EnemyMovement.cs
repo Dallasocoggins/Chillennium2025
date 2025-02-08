2bb3ab1d2de578a78c57a485767bcbd798a73de7
Dallasocoggins <74991383+Dallasocoggins@users.noreply.github.com>
2025-02-07 22:59:57 -0600

    Enemy Teleporting

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,6 +1,12 @@
+using NUnit.Framework;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static UnityEditor.FilePathAttribute;
+using static UnityEngine.GraphicsBuffer;
 
 public class EnemyMovement : MonoBehaviour
 {
@@ -17,10 +23,15 @@ public class EnemyMovement : MonoBehaviour
     private bool isGrounded;
     private EnemyState currentState = EnemyState.Chase;
 
+    public Queue<Vector3> positionQueue = new Queue<Vector3>();
+    private float lastTeleportTime = 0;
+    private float timeTillTeleport = 3f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        StartCoroutine(RecordTransform());
     }
 
     void FixedUpdate()
@@ -46,11 +57,12 @@ public class EnemyMovement : MonoBehaviour
         float xDist = Math.Abs(this.transform.position.x - target.position.x);
         float yDist = Math.Abs(this.transform.position.y - target.position.y);
         bool shouldTeleport = yDist > 2 || xDist > 5;
-        if (shouldTeleport)
+        if (shouldTeleport && currentState == EnemyState.Chase)
         {
             currentState = EnemyState.Teleport;
+            SetTimeToTeleport();
         }
-        else
+        else if (!shouldTeleport)
         {
             currentState = EnemyState.Chase;
         }
@@ -63,7 +75,27 @@ public class EnemyMovement : MonoBehaviour
 
     private void Teleport()
     {
-        print("Teleport");
+        if (Time.time - lastTeleportTime >= timeTillTeleport && positionQueue.Count > 0)
+        {
+            this.transform.position = positionQueue.Dequeue();
+
+            SetTimeToTeleport();
+            lastTeleportTime = Time.time;
+
+            print("Time till teleport" + timeTillTeleport);
+        }
+    }
+
+    private void SetTimeToTeleport()
+    {
+        if (positionQueue.Count > 0)
+        {
+            float xDist = Math.Abs(this.transform.position.x - positionQueue.Peek().x);
+            float yDist = Math.Abs(this.transform.position.y - positionQueue.Peek().y);
+            float dist = xDist + yDist;
+
+            timeTillTeleport = dist / moveSpeed;
+        }
     }
 
     private void Freeze()
@@ -80,6 +112,18 @@ public class EnemyMovement : MonoBehaviour
         else
         {
             currentState = EnemyState.Chase;
+        }
+    }
+
+    IEnumerator RecordTransform()
+    {
+        while (true) // Runs indefinitely
+        {
+            if (target != null)
+            {
+                positionQueue.Enqueue(target.position);
+            }
+            yield return new WaitForSeconds(1f);
         }
     }
 }
