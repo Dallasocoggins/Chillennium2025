15a5d3809ed73cad109986bfc31924843920cb33
Dallasocoggins <74991383+Dallasocoggins@users.noreply.github.com>
2025-02-09 12:43:01 -0600

    Video and background wip

---
+++ Assets/Video.cs
@@ -0,0 +1,22 @@
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+using UnityEngine;
+
+public class Video : MonoBehaviour
+{
+
+    VideoPlayer video;
+
+    void Awake()
+    {
+        video = GetComponent<VideoPlayer>();
+        video.Play();
+        video.loopPointReached += CheckOver;
+    }
+
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+}
